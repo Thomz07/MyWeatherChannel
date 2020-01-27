13b958e2e2dd040da32b045b7e444196ca77bb1b
Thomas <thom.laffay@gmail.com>
2020-01-27 15:00:07 +0100

    Initial commit

---
+++ arduino code/load_file.ino
@@ -0,0 +1,48 @@
+#define tBufferEnvoi 64 /// taille de buffer TCPIP
+
+void load_file(String fichier,EthernetClient cl)
+{
+    File myFile = SD.open(fichier);
+    if (myFile) {
+    byte clientBuf[64];
+    byte clientCount = 0;
+    while (myFile.available())
+    {
+    clientBuf[clientCount] = myFile.read();
+    clientCount++;
+    if(clientCount > 63)
+    {
+    cl.write(clientBuf,64);
+    clientCount = 0;
+    }
+    }
+    if(clientCount > 0) cl.write(clientBuf,clientCount);
+    myFile.close();
+    }
+}
+
+void print_message(String tabChar, boolean new_ligne,EthernetClient cl)
+{
+        char m_Buff_Envoi[tBufferEnvoi];
+        byte m_cursBuffer=0;/// curseur de buffer
+        byte i=0,x=0;
+        while( i < tabChar.length())
+        {
+        if(m_cursBuffer==tBufferEnvoi)/// on verifie que l'on ne depasse pas la taille du buffer
+        {
+        // transmission
+        cl.write(m_Buff_Envoi,m_cursBuffer);/// on envoi le buffer
+        delay(1);
+        m_cursBuffer=0;/// on reposition notre curseur en debut de buffer
+        //********************
+        }
+        else
+        {
+        m_Buff_Envoi[m_cursBuffer]= char(tabChar[i]);/// ecrire dans le buffer le caractere reçu
+        m_cursBuffer++; ///sinon on incremente notre curseur
+        i++; ///lire le caractere suivant
+        }
+        }
+        if(m_cursBuffer > 0) cl.write(m_Buff_Envoi,m_cursBuffer); // envoie des dernieres donnees
+        if (new_ligne== true) cl.println();
+}
---
+++ arduino code/web.ino
@@ -0,0 +1,203 @@
+
+#include <SPI.h>
+#include <Ethernet2.h>
+#include <SD.h>
+
+
+
+int temperature=0;
+int humidite=0;
+int pression=0;
+int altitude=0;
+int luminosite=0;
+int directionVent=0;
+int vitesseVent=0;
+int pluie=0;
+int pluieJour=0;
+int rafraichir=0;
+int action =0;
+String password="alpine";
+
+
+
+
+
+String ethernet_buffer;
+
+byte mac[] = {0xA8, 0x61, 0x0A, 0xAE, 0x17, 0xDD};
+IPAddress ip(172, 25, 12, 6); 
+EthernetServer server(80);
+File webFile;
+
+
+void setup()
+{
+ 
+          
+  Serial.begin(9600);
+  delay(3000);
+   
+     Serial.println("Initializing SD card...");
+    if (!SD.begin(4)) {
+        Serial.println("ERROR - SD card initialization failed!");
+        return;    // init failed
+    }
+    Serial.println("SUCCESS - SD card initialized.");
+    // check for index.htm file
+    if (!SD.exists("index1.htm") && !SD.exists("index2.htm")) {
+        Serial.println("ERROR - Can't find file!");
+        return;  // can't find index file
+    }
+    else Serial.println("Your files has been found !");
+    
+    
+    ethernet_buffer ="";
+  
+    
+    Ethernet.begin(mac, ip);  
+    server.begin();          
+}
+
+
+
+void loop()
+{
+EthernetClient client = server.available();
+
+    if (client) {
+        boolean currentLineIsBlank = true;
+        while (client.connected()) {
+            if (client.available()) {
+                char c = client.read();
+                ethernet_buffer += c;
+                Serial.write(c);
+
+                if (c == '\n' && currentLineIsBlank) {
+
+                     action =0;
+
+         
+                     
+                     if(ethernet_buffer.indexOf("totality")>0) action=1;    // rafraichissement des données
+                    // if(ethernet_buffer.indexOf("analog_val")>0) action=2; 
+                    // if(ethernet_buffer.indexOf("transmettre")>0) action=4;
+                    
+
+                     if(action !=0)
+                     {
+                        print_message("HTTP/1.1 200 OK",true,client);
+                        print_message("Content-Type: text/xml",true,client);
+                        print_message("Connection: keep-alive",true,client);
+                        client.println();
+                        XML_response(client, action);                      // générer le fichier XML
+                    }
+                    
+                    else {  
+                        
+                        if(ethernet_buffer.indexOf("password.js")>0)
+                          {
+                          print_message("HTTP/1.1 200 OK",true,client);
+                          print_message("Content-Type: text/javascript",true,client);
+                          print_message("Connection: keep-alive",true,client);
+                          client.println();
+                        
+                          load_file("password.js",client);                    // chargement password.js
+                            
+                          }
+                          else if(ethernet_buffer.indexOf("parallax.js")>0)
+                            {
+                        print_message("HTTP/1.1 200 OK",true,client);
+                        print_message("Content-Type: text/javascript",true,client);
+                        print_message("Connection: keep-alive",true,client);
+                        client.println();
+                        
+                        load_file("parallax/parallax.js", client);      // chargement parallax.js 
+                              
+                            }
+
+                 
+
+                          else
+                          {
+                            print_message("HTTP/1.1 200 OK",true,client);
+                            print_message("Content-Type: text/html",true,client);
+                            print_message("Connection: keep-alive",true,client);
+                            client.println();
+                            
+                            if(ethernet_buffer.indexOf("index2.htm")>0){
+                              load_file("pages/index2.htm",client);      
+                            }
+                                 else {
+                                  load_file("index1.htm",client);                        // chargement de la page web 
+                                 }
+                          }
+                        
+                        
+                        
+                        
+
+                                            
+                        
+                        if (webFile) {
+                            while(webFile.available()) {
+                                                        client.write(webFile.read()); // lecture sur carte SD
+                                                        }
+                            webFile.close();
+                            }
+                         }
+                    
+
+                    ethernet_buffer="";
+                    break;
+                }
+                if (c == '\n') {currentLineIsBlank = true;} 
+                else if (c != '\r') {currentLineIsBlank = false;}
+            } 
+        } 
+        delay(1);   
+        client.stop(); 
+    } 
+  
+}
+
+
+
+
+
+//**************************************************
+// Fichier XML
+void XML_response(EthernetClient cl, int ordre)
+{
+
+temperature = random(13, 25);
+humidite = 12;
+
+cl.print("<?xml version = \"1.0\" ?>");
+cl.print("<inputs>");
+
+cl.print("<temperature>");
+cl.print(temperature);
+cl.print("</temperature>");
+
+cl.print("<humidite>");
+cl.print(humidite);
+cl.print("</humidite>");
+
+cl.print("<password>");
+cl.print(password);
+cl.print("</password>");
+     
+cl.print("</inputs>");
+
+}
+
+
+
+
+
+
+
+//**************************************************
+
+
+
